20ea85273c72abf2a8f23340cbe7ee84bf67ad67
Simone Balducci <simone.balducci00@gmail.com>
2025-08-20 10:53:14 +0200

    Add CPU implementation of association map

---
+++ include/xstl/cpu/detail/association_map.hpp
@@ -0,0 +1,87 @@
+
+
+#pragma once
+
+#include "xstl/cpu/association_map.hpp"
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <utility>
+
+namespace xstd {
+
+  template <typename T>
+  association_map<T>::Extents association_map<T>::extents_impl() const {
+    return {m_nvalues, m_nkeys};
+  }
+
+  template <typename T>
+  association_map<T>::iterator association_map<T>::find_impl(key_type key) {
+    return m_data.values.data() + m_data.keys[key];
+  }
+  template <typename T>
+  association_map<T>::const_iterator association_map<T>::find_impl(key_type key) const {
+    return m_data.values.data() + m_data.keys[key];
+  }
+
+  template <typename T>
+  association_map<T>::size_type association_map<T>::count_impl(key_type key) const {
+    return m_data.keys[key + 1] - m_data.keys[key];
+  }
+
+  template <typename T>
+  bool association_map<T>::contains_impl(key_type key) const {
+    if (key < 0 || static_cast<size_type>(key) >= m_nkeys)
+      throw std::out_of_range("Key is out of range.");
+
+    return m_data.keys[key + 1] > m_data.keys[key];
+  }
+
+  template <typename T>
+  association_map<T>::iterator association_map<T>::lower_bound_impl(key_type key) {
+    return m_data.values.data() + m_data.keys[key];
+  }
+  template <typename T>
+  association_map<T>::const_iterator association_map<T>::lower_bound_impl(key_type key) const {
+    return m_data.values.data() + m_data.keys[key];
+  }
+
+  template <typename T>
+  association_map<T>::iterator association_map<T>::upper_bound_impl(key_type key) {
+    return m_data.values.data() + m_data.keys[key + 1];
+  }
+  template <typename T>
+  association_map<T>::const_iterator association_map<T>::upper_bound_impl(key_type key) const {
+    return m_data.values.data() + m_data.keys[key + 1];
+  }
+
+  template <typename T>
+  std::pair<typename association_map<T>::iterator, typename association_map<T>::iterator>
+  association_map<T>::equal_range_impl(key_type key) {
+    return std::make_pair(m_data.values.data() + m_data.keys[key],
+                          m_data.values.data() + m_data.keys[key + 1]);
+  }
+  template <typename T>
+  std::pair<typename association_map<T>::const_iterator, typename association_map<T>::const_iterator>
+  association_map<T>::equal_range_impl(key_type key) const {
+    return std::make_pair(m_data.values.data() + m_data.keys[key],
+                          m_data.values.data() + m_data.keys[key + 1]);
+  }
+
+  template <typename T>
+  void association_map<T>::fill_impl(std::span<association_map<T>::key_type> keys,
+                                     std::span<association_map<T>::mapped_type> values) {
+    std::vector<key_type> accumulator(m_nkeys, 0);
+    std::for_each(keys.begin(), keys.end(), [&](key_type key) { accumulator[key]++; });
+    std::vector<key_type> temporary_keys(m_nkeys + 1);
+    temporary_keys[0] = 0;
+    std::inclusive_scan(accumulator.begin(), accumulator.end(), temporary_keys.begin() + 1);
+    std::copy(temporary_keys.begin(), temporary_keys.end(), m_data.keys.begin());
+    for (auto i = 0u; i < keys.size(); ++i) {
+      auto& offset = temporary_keys[keys[i]];
+      m_data.values[offset] = values[i];
+      ++offset;
+    }
+  }
+
+}  // namespace xstd
