f50ccea8c1e2f65f4733a0700b10e42ee8ef1833
Simone Balducci <93096843+sbaldu@users.noreply.github.com>
2025-11-11 19:47:38 +0100

    Use `__CUDACC__` to conditionally include the CUDA header (#17)

--- include/xstl/xstl.hpp
+++ include/xstl/xstl.hpp
@@ -2,9 +2,11 @@
 #pragma once
 
 #include "xstl/cpu/association_map.hpp"
-#ifdef CUDA_ARCH
+
+#ifdef __CUDACC__
 #include "xstl/cuda/association_map.hpp"
 #endif
+
 #if defined(ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED) || defined(ALPAKA_ACC_CPU_B_TBB_T_SEQ_ENABLED) || \
     defined(ALPAKA_ACC_CPU_B_OMP2_T_SEQ_ENABLED) || defined(ALPAKA_ACC_GPU_CUDA_ENABLED) ||       \
     defined(ALPAKA_ACC_GPU_HIP_ENABLED) || defined(ALPAKA_ACC_SYCL_ENABLED)
