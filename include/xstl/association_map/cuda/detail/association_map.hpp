9cd068ee43a86d2f157fcbc929b6d480fc888414
Simone Balducci <simone.balducci00@gmail.com>
2025-11-24 19:57:37 +0100

    Fix include paths in CUDA headers

--- include/xstl/association_map/cuda/detail/association_map.hpp
+++ include/xstl/association_map/cuda/detail/association_map.hpp
@@ -2,8 +2,8 @@
 #pragma once
 
 #include "xstl/core/cuda/device_unique.hpp"
-#include "xstl/cuda/association_map/association_map.hpp"
-#include "xstl/cuda/association_map/detail/kernels.cuh"
+#include "xstl/association_map/cuda/association_map.hpp"
+#include "xstl/association_map/cuda/detail/kernels.cuh"
 #include <span>
 #include <thrust/execution_policy.h>
 #include <thrust/async/scan.h>
